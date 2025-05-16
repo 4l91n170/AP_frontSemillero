ddb81589bc19d368cdf9ed3d22cebec7da26ef9b
4l91n170 <Nicolasmontoya28@hotmail.com>
2025-05-16 16:57:42 -0500

    Se realiza la integracion de los modales al formulario docente, de igual forma se realiza correciones en el router link para que haya un direccionamiento correcto entre el formulario docente y formulario estudiante

--- src/app/Componentes/pop-exitoso/pop-exitoso.component.ts
+++ src/app/Componentes/pop-exitoso/pop-exitoso.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { Subscription } from 'rxjs';
+import { PopupType, ServiceModalDocenteService } from '../../Servicios/service-modal-docente.service';
+declare var bootstrap: any;
 @Component({
   selector: 'app-pop-exitoso',
   standalone: false,
@@ -7,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrl: './pop-exitoso.component.css'
 })
 export class PopExitosoComponent {
-
+ textoExito= '';
+  mostrarModal = false;
+  private subscription: Subscription = new Subscription();
+  
+  constructor(public _serviceModalDocente: ServiceModalDocenteService) {}
+  
+  ngOnInit() {
+    this.subscription = this._serviceModalDocente.popupState$.subscribe(state => {
+      if (state.show && state.type === PopupType.EXITO) {
+        this.textoExito = state.message;
+        this.abrirModal();
+      }
+    });
+  }
+  
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+  
+  abrirModal() {
+    const modal = document.getElementById('successModal');
+    if (modal) {
+      // Crear directamente una instancia de Modal
+      const bootstrapModal = new bootstrap.Modal(modal);
+      bootstrapModal.show();
+      
+      // Configurar el cierre del modal para que actualice el servicio
+      modal.addEventListener('hidden.bs.modal', () => {
+        this._serviceModalDocente.hidePopup()
+      }, { once: true });
+    }
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormularioEstudianteComponent } from './Componentes/formulario-estudiante/formulario-estudiante.component';
+import { FormularioDocenteComponent } from './Componentes/formulario-docente/formulario-docente.component';
 
-const routes: Routes = [{ path: ':id', component: FormularioEstudianteComponent }];
+const routes: Routes = [  { 
+    path: 'formulario/:idQr', 
+    component: FormularioEstudianteComponent 
+  },
+  { path: 'GenerarQr', component: FormularioDocenteComponent },  // Ruta /home
+  { path: '', redirectTo: '/GenerarQr', pathMatch: 'full' },
+  { 
+    path: ':idQr', 
+    redirectTo: 'formulario/:idQr', 
+    pathMatch: 'full' 
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
